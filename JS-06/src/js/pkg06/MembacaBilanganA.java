de21b4dc570f29fe9f9324a1266ac51a083f9dd1
21343002 <112646969+21343002@users.noreply.github.com>
2022-10-06 10:19:32 +0700

    Add files via upload

---
+++ JS-06/src/js/pkg06/MembacaBilanganA.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package js.pkg06;
+
+import java.util.Scanner;
+
+/**
+ *
+ * Created by 21343002_Aini Rizkina
+ */
+public class MembacaBilanganA {
+    public static void main(String[] args) {
+        int angka;
+        
+        Scanner angkaMasuk = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        angka = angkaMasuk.nextInt();
+        
+        if (angka == 1){
+            System.out.println("SATU");
+        }
+        else if (angka == 2){
+            System.out.println("DUA");
+        }
+        else if (angka == 3){
+            System.out.println("TIGA");
+        }
+        else if (angka == 4){
+            System.out.println("EMPAT");
+        }
+        else if (angka == 5){
+            System.out.println("LIMA");
+        }
+        else if (angka == 6){
+            System.out.println("ENAM");
+        }
+        else if (angka == 7){
+            System.out.println("TUJUH");
+        }
+        else if (angka == 8){
+            System.out.println("DELAPAN");
+        }
+        else if (angka == 9){
+            System.out.println("SEMBILAN");
+        }
+        else if (angka == 10){
+            System.out.println("SEPULUH");
+        }
+        else
+            System.out.println("Invalid number.");
+}
+}
+
---
+++ JS-06/src/js/pkg06/MembacaBilanganB.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package js.pkg06;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * Created by 21343002_Aini Rizkina
+ */
+public class MembacaBilanganB {
+    public static void main(String[] args) {
+        int angka;
+        
+        Scanner angkaMasuk = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        angka = angkaMasuk.nextInt();
+        
+        switch (angka) {
+            case 1:
+                System.out.println("SATU");
+                break;
+            case 2:
+                System.out.println("DUA");
+                break;
+            case 3:
+                System.out.println("TIGA");
+                break;
+            case 4:
+                System.out.println("EMPAT");
+                break;
+            case 5:
+                System.out.println("LIMA");
+                break;
+            case 6:
+                System.out.println("ENAM");
+                break;
+            case 7:
+                System.out.println("TUJUH");
+                break;
+            case 8:
+                System.out.println("DELAPAN");
+                break;
+            case 9:
+                System.out.println("SEMBILAN");
+                break;
+            case 10:
+                System.out.println("SEPULUH");
+                break;
+            default:
+                System.out.println("Invalid number.");
+        }
+    } 
+}
